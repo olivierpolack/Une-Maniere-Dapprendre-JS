9f0b8c698abebb8746b1aebd57cebab1ddef9f71
Olivier <o.polack@gmail.com>
2022-02-23 07:36:37 +0100

    Ajout fichiers initiaux

---
+++ Un-script.js
@@ -0,0 +1,12 @@
+function mafonctionAlerte(){
+  alert("C'est une alerte");
+}
+
+function mafonctionVariables() {
+  var monPrenom = "Olivier";
+  var monNom = "Polack";
+  var age = 44;
+  alert("Mon prenom est: "+ monPrenom + " et mon nom est : "+ monNom + ".");
+  alerte("Et j ai: "+ age.ToString()); // Cette fonction échoue. Pourquoi?
+
+}
