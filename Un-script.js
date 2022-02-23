69ebab4862738605a6476bfa912bc99674726449
Olivier <o.polack@gmail.com>
2022-02-23 19:20:38 +0100

    Maj chap 1 à 10

--- Un-script.js
+++ Un-script.js
@@ -10,3 +10,24 @@ function mafonctionVariables() {
   alerte("Et j ai: "+ age.ToString()); // Cette fonction échoue. Pourquoi?
 
 }
+
+function unPrompt(){
+    //alert("exec");
+    let question = "Votre espèce?";
+    let reponseParDefaut = "humaine";
+    let reponse = prompt(question, reponseParDefaut);
+    let text;
+    if ( reponse === "" ) {
+      text = "Vous n'avez rien saisi.";
+    }
+    else if ( reponse === null ) {
+      text = "Vous avez appuyé sur Annuler";
+    }
+    else if ( reponse === reponseParDefaut){
+      text = "Votre réponse est celle proposée par défaut: "+ reponse;
+    }
+    else {
+      text = "Vous avez saisi: "+ reponse+ ". Vous êtes un extra-terrestre mais bienvenu quand-même";
+    }
+    document.getElementById("reponseDuPrompt").innerHTML = text;
+}
